f6905bc902fb3db5bda56101316da8c9ba8367e3
iguiIllanes <igui10082002@gmail.com>
2023-09-04 11:41:06 -0400

    Add javadoc

--- 02_LinkedLists/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LinkedList"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"LinkedList.Node"}];updateSearchResults();
